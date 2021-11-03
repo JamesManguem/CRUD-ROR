eb6805266481a4f0c092685875f73e7228137af3
JamesManguem <18090067@alumno.utmetropolitana.edu.mx>
2021-11-03 15:47:36 -0600

    CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,56 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    # @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+
+      redirect_to action: 'index'
+
+  end
+
+
+
+  def update
+    # @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to action: 'index'
+  end
+
+  def destroy
+
+    # @user = User.find(params[:id])
+    @user.destroy
+    redirect_to action: 'index'
+
+  end
+
+  def show
+  end
+
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:lastname,:email,:phone,:image)
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,5 @@ class User < ApplicationRecord
   validates_presence_of   :lastname, :message => 'Debe ingresar su apellido.'
   validates :email, presence:true,format: {with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ , message:"Formato de correo invalido" }
   validates :phone, presence: true, format: {with: /\A\d{10}\z|\A\d{4}-{1}\d{6}\z/, message: "El numero de telefono debe tener 10 digitos"}
+  has_one_attached :image
 end
